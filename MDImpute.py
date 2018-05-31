3d28e65027df71c515c495a1c7d7932455e92f2f
Chu Luo <mr.cluo29@gmail.com>
2018-05-31 15:12:08 +1000

    Update MDImpute.py

--- MDImpute.py
+++ MDImpute.py
@@ -4,6 +4,19 @@
 
 # first, mode, mean, median, hot deck impute.
 
+def mean_impute(inputSet, column_id, label):
+    missing_label = int(label)
+    
+def median_impute(inputSet, column_id, label):
+    missing_label = int(label)
+
+def mode_impute(inputSet, column_id, label):
+    missing_label = int(label)
+
+def hot_deck_impute(inputSet, column_id, label):
+     missing_label = int(label)
+
+
 # then regression impute
 
 # finally, our feature impact impute
