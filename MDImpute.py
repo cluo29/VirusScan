cc63a3bbe3761fa9644df099fea6b9c20cc64f14
Chu Luo <mr.cluo29@gmail.com>
2018-06-11 20:43:49 +1000

    Update MDImpute.py

--- MDImpute.py
+++ MDImpute.py
@@ -3,7 +3,8 @@
 import numpy as np
 from scipy import stats
 from sklearn import *
-
+from sklearn import preprocessing
+from sklearn.metrics import *
 # first, mode, mean, median, hot deck impute.
 
 def mean_impute(inputSet, column_id, label):
@@ -87,7 +88,7 @@ def lr_impute(inputSet, column_id, label):
 
 def knn_impute(inputSet, column_id, label):
     # normalization needed
-    
+
     missing_label = int(label)
     a = np.array(inputSet)
     valid_set = a[a[:, column_id] != missing_label]
@@ -129,12 +130,16 @@ def MLP_impute(inputSet, column_id, label):
 
     regr = neural_network.MLPRegressor()
 
-    # Train the model using the training sets
-    regr.fit(X_train, Y_train)
 
     X_test = np.delete(invalid_set, column_id, 1)
 
-    Y_test = regr.predict(X_test)
+    #X_trainP, X_testP = MDI_preprocess(X_train, X_test)
+    X_trainP, X_testP = X_train, X_test
+
+    # Train the model using the training sets
+    regr.fit(X_trainP, Y_train)
+
+    Y_test = regr.predict(X_testP)
 
     rows = len(a)
     imputation_count = 0
@@ -144,14 +149,47 @@ def MLP_impute(inputSet, column_id, label):
             imputation_count = imputation_count + 1
 
     return a
+
+
+
 # finally, our feature impact impute
+def FI_impute():
+    print('TODO')
+
+# preprocessing "normalization" code, scaling to [0,1]
+def MDI_preprocess(X_train, X_test):
+    # input X train set and test set
+    # output preprocessed them
+    # results show MLP doesn;t like this...
+    min_max_scaler = preprocessing.MinMaxScaler()
+    AllX = np.concatenate((X_train, X_test), axis=0)
+    min_max_scaler.fit(AllX)
+
+    output_X_train = min_max_scaler.transform(X_train)
+    output_X_test = min_max_scaler.transform(X_test)
+    return output_X_train, output_X_test
+
+# evaluation of MDI, using MAE and MSE
+def MDI_eva(inputSet, column_id, label, imputation, ground_truth):
+    missing_label = int(label)
+    a = np.array(inputSet)
+
+    y_true = ground_truth[a[:, column_id] == missing_label, column_id]
+    y_pred = imputation[a[:, column_id] == missing_label, column_id]
+    MAE = mean_absolute_error(y_true, y_pred)
+    MSE = mean_squared_error(y_true, y_pred)
+    print('MAE = ', MAE)
+    print('MSE = ', MSE)
+
 
 # test
+gt = np.array([[1, 2, 3], [6, 6, 12], [6, 6, 12], [6, 6, 12],[6, 6, 12],[6, 6, 12],[6, 6,12],[6, 6, 12],[6, 6, 12],[6, 6, 12],[6, 6, 12],[6, 6, 12],[6, 6, 12],[6, 6, 12], [6, 6, 12]], dtype='f')
 
-a = np.array([[1, 2, 5], [6, 6, -1], [6, 6, 5], [6, 6, 6],[6, 6, 6],[6, 6, 6],[6, 6, 6],[6, 6, 6],[6, 6, 6],[6, 6, 6],[6, 6, 6],[6, 6, 6],[6, 6, 6],[6, 6, 6], [6, 6, -1]], dtype='f')
+a = np.array([[1, 2, 3], [6, 6, -1], [6, 6, 12], [6, 6, 12],[6, 6, 12],[6, 6, 12],[6, 6,12],[6, 6, 12],[6, 6, 12],[6, 6, 12],[6, 6, 12],[6, 6, 12],[6, 6, 12],[6, 6, 12], [6, 6, -1]], dtype='f')
 
-print(a)
+
+b = mean_impute(a, 2, -1)
+MDI_eva(a, 2, -1, b, gt)
 
 b = MLP_impute(a, 2, -1)
-
-print(b)
+MDI_eva(a, 2, -1, b, gt)
